1f106efac368e77ab8ab6d1eabab94016fa6270a
Subrata Halder <subrata.haldar@codeclouds.com>
2025-06-21 15:27:04 +0530

    Changes in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -111,7 +111,7 @@ const Footer = () => {
     <div className="modal fade" id="loginmodal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered">
         <div className="modal-content">
-          <div className="modal-header p-0">
+          <div className="modal-header">
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
